3d707ed61fe3fcc0c81dfa554863593b3985bab8
v.rieznikova <v.rieznikova@nodeart.io>
2021-12-06 04:58:03 +0200

    xor added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const fs = require('fs')
 
 const salsa20 = fs.readFileSync('salsa20.csv', 'utf8' , (err, data) => data).split('\r\n')
+
+const hexToBytes = hex => {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+  bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+const bytes = salsa20.map(el => hexToBytes(el))
+fs.writeFileSync('bytes.csv', bytes.join('\r\n'))
+
+const xor = (char, line, position, discover) => 
+  String.fromCharCode(
+    char.charCodeAt(0)^
+      hexToBytes(salsa20[line])[position]^
+      hexToBytes(salsa20[discover])[position])
+
+const discover = (char, line, position) => bytes.map((el, i) => {
+  el[position] = i === line ? char : xor(char, line, position, i);
+  return el;
+})
+
+fs.writeFileSync('result.csv', discover('T', 2, 0).join('\r\n'))
