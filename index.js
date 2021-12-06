8a56e4f6b1743a0b7f609c57b14c6cc2e834ad84
v.rieznikova <v.rieznikova@nodeart.io>
2021-12-06 13:15:10 +0200

    ready

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
 
-const salsa20 = fs.readFileSync('salsa20.csv', 'utf8' , (err, data) => data).split('\r\n')
+const salsa20 = fs.readFileSync('salsa20.txt', 'utf8' , (err, data) => data).split('\r\n')
 
-const hexToBytes = hex => {
-  for (var bytes = [], c = 0; c < hex.length; c += 2)
-  bytes.push(parseInt(hex.substr(c, 2), 16));
-  return bytes;
-}
+const hexToBytes = hex => 
+  Array.from(hex)
+       .reduce((acc, curr, i, arr) => 
+          i % 2 ? 
+            acc : 
+            [...acc, parseInt(curr + arr[i + 1], 16)], 
+          [])
 
 const bytes = salsa20.map(el => hexToBytes(el))
 fs.writeFileSync('bytes.csv', bytes.join('\r\n'))
@@ -17,31 +19,12 @@ const xor = (char, line, position, discover) =>
       hexToBytes(salsa20[line])[position]^
       hexToBytes(salsa20[discover])[position])
 
-// console.log(String.fromCharCode(
-//   'T'.charCodeAt(0)^
-//     hexToBytes(salsa20[2])[0]^
-//     hexToBytes(salsa20[0])[2]))
-
 const discover = (char, line, position) => bytes.map((el, i) => {
   if(el[position]) {
     el[position] = i === line ? char : xor(char, line, position, i);
   }
-  return el;
+  return el.join(';');
 })
-
-// const magic = (char, line, position) => {
-//   const code = bytes[line][position]
-//   bytes.map(elLine => {
-//     elLine.map(elPos => {
-//       if (elPos === code) {
-//         discover(char, elLine, elPos)
-//       } else {
-        
-//       }
-//     })
-//   })
-//   // fs.writeFileSync('result.csv', discover('T', 2, 0).join('\r\n'))
-// }
 
 fs.writeFileSync('result.csv', discover('T', 2, 0).join('\r\n'))
 fs.writeFileSync('result.csv', discover('h', 2, 1).join('\r\n'))
@@ -77,8 +60,21 @@ fs.writeFileSync('result.csv', discover('t', 5, 30).join('\r\n'))
 fs.writeFileSync('result.csv', discover('d', 0, 31).join('\r\n'))
 fs.writeFileSync('result.csv', discover(' ', 0, 32).join('\r\n'))
 fs.writeFileSync('result.csv', discover('h', 9, 33).join('\r\n'))
-fs.writeFileSync('result.csv', discover('i', 9, 34).join('\r\n'))
-// fs.writeFileSync('result.csv', discover('t', 14, 35).join('\r\n'))
-
+fs.writeFileSync('result.csv', discover('t', 8, 34).join('\r\n'))
+fs.writeFileSync('result.csv', discover('e', 8, 35).join('\r\n'))
+fs.writeFileSync('result.csv', discover('r', 8, 36).join('\r\n'))
+fs.writeFileSync('result.csv', discover(' ', 8, 37).join('\r\n'))
+fs.writeFileSync('result.csv', discover(' ', 15, 38).join('\r\n'))
+fs.writeFileSync('result.csv', discover('t', 16, 39).join('\r\n'))
+fs.writeFileSync('result.csv', discover('a', 11, 40).join('\r\n'))
+fs.writeFileSync('result.csv', discover('v', 11, 41).join('\r\n'))
+fs.writeFileSync('result.csv', discover('e', 11, 42).join('\r\n'))
+fs.writeFileSync('result.csv', discover(',', 8, 43).join('\r\n'))
+fs.writeFileSync('result.csv', discover('h', 15, 44).join('\r\n'))
+fs.writeFileSync('result.csv', discover('m', 0, 45).join('\r\n'))
+fs.writeFileSync('result.csv', discover('e', 0, 46).join('\r\n'))
+fs.writeFileSync('result.csv', discover(',', 0, 47).join('\r\n'))
+const text = fs.readFileSync('result.csv', 'utf8' , (err, data) => data).split('\r\n').map(el => el.split(';').join('')).join('\r\n')
+fs.writeFileSync('text.txt', text)
 
 
